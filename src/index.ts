809aecbbe23e1c04e77a4e24667b22bae1a61d14
Kuldeep2602 <e22cseu1637@bennett.edu.in>
2025-07-04 15:32:28 +0530

    transaction

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,59 @@ const pgClient = new Client(process.env.DATABASE_URL);
 
 pgClient.connect();
 
-app.post("/signup" , async(req, res) =>{
+// app.post("/signup" , async(req, res) =>{
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     const email = req.body.email;
+
+//     const city = req.body.city;
+//     const country = req.body.country;
+//     const street = req.body.street;
+//     const pincode = req.body.pincode;
+
+//     try{
+//         //const response =  await pgClient.query(`INSERT INTO "User" (username, password) VALUES ('${username}', '${password}')`);
+            
+//         const InsertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id;`
+//         const AddQuery = `INSERT INTO addresses (city, country, street, pincode) VALUES ($1, $2, $3, $4)`;
+
+        
+        
+         
+//        await pgClient.query("BEGIN;")
+        
+        
+//         const response = await pgClient.query(InsertQuery, [username, password, email]);
+//         const userId = response.rows[0].id; // Get the inserted user ID
+//        console.log("User ID:", userId);
+//         const res1 = await pgClient.query(AddQuery, [city, country, street, pincode]);
+        
+
+//         await pgClient.query("COMMIT;")
+
+
+//         //const response =  await pgClient.query('INSERT INTO "User" (username, password) VALUES ($1, $2)', [username, password]);
+//     res.json({
+//             message: "User created successfully"
+//     })
+
+//     }catch(e : any){
+//         console.log(e);
+//         res.json({
+//             message : 'error creating user',
+//             error: e.message
+//         })
+//     }
+
+    
+
+// })
+
+
+
+
+
+app.post("/signup", async(req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
@@ -42,34 +94,40 @@ app.post("/signup" , async(req, res) =>{
     const street = req.body.street;
     const pincode = req.body.pincode;
 
-    try{
-        //const response =  await pgClient.query(`INSERT INTO "User" (username, password) VALUES ('${username}', '${password}')`);
+    try {
+        // Start transaction
+        await pgClient.query("BEGIN;");
+        
+        try {
+            const InsertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id;`
+            const AddQuery = `INSERT INTO addresses (user_id, city, country, street, pincode) VALUES ($1, $2, $3, $4, $5)`;
             
-        const InsertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id;`
-        const response = await pgClient.query(InsertQuery, [username, password, email]);
-        
-        const userId = response.rows[0].id; // Get the inserted user ID
-        console.log("User ID:", userId);
-        
-        const AddQuery = `INSERT INTO addresses (city, country, street, pincode) VALUES ($1, $2, $3, $4)`;
+            const response = await pgClient.query(InsertQuery, [username, password, email]);
+            const userId = response.rows[0].id; // Get the inserted user ID
+            console.log("User ID:", userId);
+            const res1 = await pgClient.query(AddQuery, [ userId, city, country, street, pincode]);
+            
+            // Commit transaction
+            await pgClient.query("COMMIT;");
 
-        
-        const res1 = await pgClient.query(AddQuery, [city, country, street, pincode]);
-        
-        //const response =  await pgClient.query('INSERT INTO "User" (username, password) VALUES ($1, $2)', [username, password]);
-    res.json({
-            message: "User created successfully"
-    })
-
-    }catch(e : any){
+            
+            
+            res.json({
+                message: "User created successfully"
+            });
+        } catch (err) {
+            // Rollback transaction if any error occurs
+            await pgClient.query("ROLLBACK;");
+            throw err; // Re-throw to be caught by the outer catch block
+        }
+    } catch (e: any) {
+        console.log(e);
         res.json({
-            message : 'error creating user',
+            message: 'error creating user',
             error: e.message
-        })
+        });
     }
+});
 
-    
 
-})
-
-app.listen(3000);
+ app.listen(3000);
